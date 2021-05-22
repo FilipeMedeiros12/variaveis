ce95110c47c4a9d3249bcc8ea63d6efe0ba167db
filipemedeiros12 <filipemedeiros144@gmail.com>
2021-05-22 12:07:35 -0300

    filipe

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-var valor = 10;
-var valor2 = 12;
-alert(valor + valor2);
-var nome = prompt('digite seu nome:');
-alert(nome + ' , seja bem vindo')
-var anoAtual = 2021;
-var anoNascimento = prompt('Digite o ano em que você nasceu');
-var idade = anoAtual - anoNascimento;
-alert('Sua idade é: ' + idade +  ' anos');
+//  var valor = 10;
+// var valor2 = 12;
+// alert(valor + valor2);
+// var nome = prompt('digite seu nome:');
+// alert(nome + ' , seja bem vindo')
+// var anoAtual = 2021;
+// var anoNascimento = prompt('Digite o ano em que você nasceu');
+// var idade = anoAtual - anoNascimento;
+// alert('Sua idade é: ' + idade +  ' anos');
+
+
+var nota = prompt('Primeira nota')
+var nota2 = prompt('Segunda nota')
+var nota3 = prompt('Terceira nota')
+var total = (parseInt(nota) + parseInt(nota2) + parseInt(nota3) )/3
+
+alert('Nota total: ' + total)
