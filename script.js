15719f6e1d031e4c1f5618fa21db9a6ab97fde45
filipemedeiros12 <filipemedeiros144@gmail.com>
2021-05-22 10:46:41 -0300

    filipe

---
+++ script.js
@@ -0,0 +1,9 @@
+var valor = 10;
+var valor2 = 12;
+alert(valor + valor2);
+var nome = prompt('digite seu nome:');
+alert(nome + ' , seja bem vindo')
+var anoAtual = 2021;
+var anoNascimento = prompt('Digite o ano em que você nasceu');
+var idade = anoAtual - anoNascimento;
+alert('Sua idade é: ' + idade +  ' anos');
